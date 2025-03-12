cbe5dbba3b6e0c52b8a2006a899464a8d1d80f3c
Qreuff <anatoliyhonor26@gmail.com>
2025-03-12 14:52:05 +0700

    reshresh base.py

--- base.py
+++ base.py
@@ -1,3 +1,3 @@
-def merge_sort(m):
-"""Sort list m, using merge sort"""
+12312312312def merge_sort(m):
+"""Sort list m, 21321312321using merge sort"""
 return m.sort() # TODO: Replace with actual implementation
